9f1cd965b1821daa78f0bba1553b4368098d44d4
Angelinl00 <105888891+Angelinl00@users.noreply.github.com>
2023-07-03 11:14:13 +0100

    test SmalVideoComponent

--- src/components/channelContent/SmalVideo.jsx
+++ src/components/channelContent/SmalVideo.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function SmalVideo({smalVideo}) {
+
+  useEffect(() => {
+    console.log(smalVideo)
+  })
+
   return (
     <div className='h-auto w-full pl-[65px] py-[24px] flex '>
       <div className='h-auto w-auto relative flex flex-row '>
         <div className='h-auto w-auto relative'>
-          <img src="./images/youtube.jpg" alt="" className='relative w-[424px] h-[238px] mr-[24px] ' />
+          <img src={smalVideo.thumbnails[0].url} alt="" className='relative w-[424px] h-[238px] mr-[24px] ' />
         </div>
 
         <div className='relative w-[424px] h-[238px]  flex flex-col items-start justify-start'>
-          <p className='text-[14px] mb-[20px] '>Blind Woodturner: Turning passion into fine art</p>
-          <p className='text-[13px] text-[#aaa] mb-[19px] '>576,969 views . 3 weeks ago</p>
-          <p className='text-start text-[14px] '>Chris Fisher, also known as the Blind Woodturner, learned his craft by listening to hundreds of hours of YouTube videos and experimenting in his workshop. Now he’s a YouTube creator himself, sells his products worldwide, and does demonstrations all around the country.</p>
+          <p className='text-[14px] mb-[20px] '>{smalVideo.title}</p>
+          <p className='text-[13px] text-[#aaa] mb-[19px] '>{smalVideo.viewCountText} . {smalVideo.publishedTimeText}</p>
+          {/* <p className='text-start text-[14px] '>Chris Fisher, also known as the Blind Woodturner, learned his craft by listening to hundreds of hours of YouTube videos and experimenting in his workshop. Now he’s a YouTube creator himself, sells his products worldwide, and does demonstrations all around the country.</p> */}
         </div>
       </div>
     </div>
